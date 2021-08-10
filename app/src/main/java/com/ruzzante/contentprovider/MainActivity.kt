6ef04bcc474f3fda2370d4fce86fb345999cf73b
rosante <rafarosante@hotmail.com>
2021-08-10 19:43:01 -0300

    Adicionando Fragment, Interfaces

--- app/src/main/java/com/ruzzante/contentprovider/MainActivity.kt
+++ app/src/main/java/com/ruzzante/contentprovider/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.ruzzante.contentprovider
 
 import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns._ID
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.ruzzante.contentprovider.`interface`.NoteClickedListener
 import com.ruzzante.contentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 import com.ruzzante.contentprovider.database.NotesProvider.Companion.URI_NOTES
 
@@ -25,14 +28,30 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
         setContentView(R.layout.activity_main)
 
         noteAdd = findViewById(R.id.btn_add)
-        noteAdd.setOnClickListener{}
+        noteAdd.setOnClickListener{
+            NotesDetailFragment().show(supportFragmentManager, "dialog1")
+        }
 
-        adapter = NotesAdapter()
+        adapter = NotesAdapter(object: NoteClickedListener{
+            override fun noteClickedItem(cursor: Cursor) {
+                val id : Long = cursor?.getLong(cursor.getColumnIndex(_ID))
+                val fragment = NotesDetailFragment.newIntance(id)
+                fragment.show(supportFragmentManager, "dialog")
+            }
+
+            override fun noteRemoveItem(cursor: Cursor?) {
+                val id : Long? = cursor?.getLong(cursor.getColumnIndex(_ID))
+                contentResolver.delete(Uri.withAppendedPath(URI_NOTES, id.toString()), null, null)
+            }
+
+        })
         adapter.setHasStableIds(true)
 
         noteRecyclerView = findViewById(R.id.rv_notes)
         noteRecyclerView.layoutManager = LinearLayoutManager(this)
         noteRecyclerView.adapter = adapter
+
+        LoaderManager.getInstance(this).initLoader(0, null, this)
     }
 
     // Instancia o que será buscado
@@ -41,11 +60,13 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
 
     // Manipula os dados recebidos
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        if (data != null){}
+        if (data != null){
+            adapter.setCursor(data)
+        }
     }
 
     // Termina a pesquisa em segundo plano
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
+        adapter.setCursor(null)
     }
 }
--- app/src/main/java/com/ruzzante/contentprovider/adapter/NotesAdapter.kt
+++ app/src/main/java/com/ruzzante/contentprovider/adapter/NotesAdapter.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.ruzzante.contentprovider.`interface`.NoteClickedListener
+import com.ruzzante.contentprovider.database.NotesDatabaseHelper.Companion.DESCRIPTION_NOTES
+import com.ruzzante.contentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 
-class NotesAdapter(): RecyclerView.Adapter<NotesViewHolder>() {
+class NotesAdapter(private val listener:NoteClickedListener): RecyclerView.Adapter<NotesViewHolder>() {
 
     private var mCursor: Cursor? = null
 
@@ -16,12 +19,18 @@ class NotesAdapter(): RecyclerView.Adapter<NotesViewHolder>() {
         NotesViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false))
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-
+        mCursor?.moveToPosition(position)
+        holder.noteTitle.text = mCursor?.getString(mCursor?.getColumnIndex(TITLE_NOTES) as Int)
+        holder.noteDescription.text = mCursor?.getString(mCursor?.getColumnIndex(DESCRIPTION_NOTES) as Int)
+        holder.noteRemove.setOnClickListener {
+            mCursor?.moveToPosition(position)
+            listener.noteRemoveItem(mCursor)
+            notifyDataSetChanged()
+        }
+        holder.itemView.setOnClickListener{listener.noteClickedItem(mCursor as Cursor)}
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = if (mCursor != null) mCursor?.count as Int else 0
 
     fun setCursor(newCursor: Cursor?){
         mCursor = newCursor
